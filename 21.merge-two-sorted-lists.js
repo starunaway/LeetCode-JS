67e1657f87246511d18cd226c4403a8ff79836cf
keeping <YKN@email.com>
2019-08-19 10:44:55 +0800

    update

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  if (!list1 || !list2) {
+    return list1 || list2;
+  }
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
+  }
+};
