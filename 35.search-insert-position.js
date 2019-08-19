ba1458fdaa17d09cfab579e60c6edb514c64c8ad
keeping <YKN@email.com>
2019-08-19 13:19:21 +0800

    complete 28 ,35

---
+++ 35.search-insert-position.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let end = nums.length;
+  let start = 0;
+  let mid;
+  for (; start <= end; ) {
+    mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  if (start > nums.length) {
+    return start - 1;
+  }
+  return start;
+};
