a509d349fcb5e55881e311fb91193133406a17b9
keepuse <YKN@email.com>
2019-09-04 17:04:27 +0800

    111 112 pass

--- 111.二叉树的最小深度.js
+++ 111.二叉树的最小深度.js
@@ -14,23 +14,23 @@
  * Testcase Example:  '[3,9,20,null,null,15,7]'
  *
  * 给定一个二叉树，找出其最小深度。
- * 
+ *
  * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
- * 
+ *
  * 说明: 叶子节点是指没有子节点的节点。
- * 
+ *
  * 示例:
- * 
+ *
  * 给定二叉树 [3,9,20,null,null,15,7],
- * 
+ *
  * ⁠   3
  * ⁠  / \
  * ⁠ 9  20
  * ⁠   /  \
  * ⁠  15   7
- * 
+ *
  * 返回它的最小深度  2.
- * 
+ *
  */
 /**
  * Definition for a binary tree node.
@@ -44,27 +44,20 @@
  * @return {number}
  */
 var minDepth = function(root) {
-	if(root === null) return 0;
-	let depth = 1;
-	let node = [root];
-	for (let i =0;i<node.length; i++){
-		let floorKids = [];
-		if(node[i].left){
-			floorKids.push(node[i].left)
-		}
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null && root.right === null) {
+    return 1;
+  }
 
-		if(node[i.right]){ 
-			floorKids.push(node[i.right])
-		}
+  let left = minDepth(root.left);
+  let right = minDepth(root.right);
 
-		}
+  if (root.left === null || root.right === null) {
+    // 如果只有一个子树为空，则深度为另一个子树的高度 +1
+    return left + right + 1;
+  }
 
-	}
-	return depth;
-    
+  return Math.min(left, right) + 1;
 };
-function minDepthKids(root){
-	if(root === null) return 0;
-	let depth =1;
-}
-
--- 112.路径总和.js
+++ 112.路径总和.js
@@ -44,4 +44,15 @@
  * @param {number} sum
  * @return {boolean}
  */
-var hasPathSum = function(root, sum) {};
+var hasPathSum = function(root, sum) {
+  if (root === null) {
+    return false;
+  }
+  if (root.left === null && root.right === null) {
+    return root.val === sum;
+  }
+  return (
+    hasPathSum(root.left, sum - root.val) ||
+    hasPathSum(root.right, sum - root.val)
+  );
+};
