507bca938554d4fd584f6d0f78919a8624dbdca9
keepuse <starunaway@gmail.com>
2019-09-25 17:49:28 +0800

    167 normal

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ *
+ * https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
+ *
+ * algorithms
+ * Easy (49.81%)
+ * Likes:    176
+ * Dislikes: 0
+ * Total Accepted:    42K
+ * Total Submissions: 84K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ *
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ *
+ * 说明:
+ *
+ *
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ *
+ *
+ * 示例:
+ *
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ *
+ */
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+
+// 可以假设每个输入只对应唯一的答案
+// 数组有序，在肯定有答案的情况下
+// 从数组两侧开始相加 超过目标则减最大值，小于目标则加最小值
+// var twoSum = function(numbers, target) {
+//   let i = 0;
+//   let j = numbers.length - 1;
+//   while (i < j) {
+//     if (numbers[i] + numbers[j] > target) {
+//       j--;
+//     } else if (numbers[i] + numbers[j] < target) {
+//       i++;
+//     } else {
+//       return [i + 1, j + 1];
+//     }
+//   }
+// };
+// console.log(twoSum([2, 7, 11, 15], 9));
